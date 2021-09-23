e42673b6e99a07d3d844efa2e2a2021a7d51d474
xBarikadex <71157911+xBarikadex@users.noreply.github.com>
2021-09-23 13:48:21 -0700

    Finished

--- social-media-text-post/src/blogPost/BlogPost.js
+++ social-media-text-post/src/blogPost/BlogPost.js
@@ -9,22 +9,28 @@ const BlogPost = ({data}) => {
 
     const [likes, setLikes] = useState(Math.round(Math.random() * 300));
     const [dislikes, setDislikes] = useState(Math.round(Math.random() * 80));
+    const [likeColor, setLikeColor] = useState("black");
+    const [dislikeColor, setDislikeColor] = useState("black");
 
     function like() {
         if (likeBoolean === false) {
             setLikes(likes + 1);
+            setLikeColor("#1F51FF");
             likeBoolean = true;
         } else if (likeBoolean === true) {
             setLikes(likes - 1);
+            setLikeColor("black");
             likeBoolean = false;
         }
     }
     function dislike() {
         if (dislikeBoolean === false) {
             setDislikes(dislikes + 1);
+            setDislikeColor("#1F51FF");
             dislikeBoolean = true;
         } else if (dislikeBoolean === true) {
             setDislikes(dislikes - 1);
+            setDislikeColor("black");
             dislikeBoolean = false;
         }
     }
@@ -33,11 +39,11 @@ const BlogPost = ({data}) => {
         <div className="blogContainer">
             <img className="profile" src={profilePicture} alt={name} />
             <h3 className="name">{name}</h3>
-            <p className="content">{text}</p>   
-            <button className="btn" id="likeBtn" onClick={() => like()}><BiLike size="2rem"/></button>
-            <p>{likes}</p>
-            <button className="btn" id="dislikeBtn" onClick={() => dislike()}><BiDislike size="2rem"/></button>
-            <p>{dislikes}</p>
+            <p className="content blogText">{text}</p>   
+            <button className="btn" id="likeBtn" onClick={() => like()}><BiLike color={likeColor}size="2rem"/></button>
+            <p className="blogText">{likes}</p>
+            <button className="btn" id="dislikeBtn" onClick={() => dislike()}><BiDislike color={dislikeColor} size="2rem"/></button>
+            <p className="blogText">{dislikes}</p>
         </div>
     );
 }
